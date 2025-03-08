8e328db0c8b0c5a2ff2757ed74b17c124fcaf3b0
Eshanth Kumar Lal Das <98878506+eshanth23@users.noreply.github.com>
2025-03-08 13:24:28 -0500

    Add files via upload

---
+++ HW3/src/test/umbcs680/metaldetector/MetalDetectionTest.java
@@ -0,0 +1,50 @@
+package umbcs680.metaldetector;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MetalDetectionTest {
+
+    @Test
+    public void Standby_To_Scanning_Transformation() {
+        MetalDetection detector = new MetalDetection();
+        detector.DetectForMotion();
+
+        assertEquals(ScanningState.class, detector.getState().getClass(),
+                "Expected: ScanningState, Actual: " + detector.getState().getClass().getSimpleName());
+    }
+
+    @Test
+    public void Scanning_To_Alarm_Transformation() {
+        MetalDetection detector = new MetalDetection();
+        detector.DetectForMotion(); // Move to scanning state
+        detector.ScanForMetal(true); // Metal detected
+
+
+        assertEquals(AlarmState.class, detector.getState().getClass(),
+                "Expected: AlarmState, Actual: " + detector.getState().getClass().getSimpleName());
+    }
+
+    @Test
+    public void Scanning_To_Standby_Transformation() {
+        MetalDetection detector = new MetalDetection();
+        detector.DetectForMotion(); // Move to scanning state
+        detector.ScanForMetal(false); // No metal detected
+
+
+        assertEquals(StandbyState.class, detector.getState().getClass(),
+                "Expected: Standby, Actual: " + detector.getState().getClass().getSimpleName());
+    }
+
+    @Test
+    public void Alarm_To_Standby_Transformation() {
+        MetalDetection detector = new MetalDetection();
+        detector.DetectForMotion();
+        detector.ScanForMetal(true); // Metal detected, goes to alarm state
+        detector.Reset(); // Reset to Standby
+
+        // Expected: IdleState.class, Actual: detector.getState().getClass()
+        assertEquals(StandbyState.class, detector.getState().getClass(),
+                "Expected: Standby, Actual: " + detector.getState().getClass().getSimpleName());
+    }
+}
