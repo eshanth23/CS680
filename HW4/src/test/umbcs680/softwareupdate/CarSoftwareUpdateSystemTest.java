20d1b03d11ac79c594d232b57d15b09010a0a1d1
Eshanth Kumar Lal Das <98878506+eshanth23@users.noreply.github.com>
2025-03-13 15:21:18 -0400

    Add files via upload

---
+++ HW4/src/test/umbcs680/softwareupdate/CarSoftwareUpdateSystemTest.java
@@ -0,0 +1,68 @@
+package umbcs680.softwareupdate;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class  CarSoftwareUpdateSystemTest {
+
+    @Test
+    void Lamborghini_Software_Updates() {
+        CarBrand lambo = new CarBrand("Lamborghini");
+
+        CarModel aventador = new CarModel("Lamborghini Aventador");
+        CarModel hurricane = new CarModel("Lamborghini Hurricane");
+
+        lambo.addObserver(aventador);
+        lambo.addObserver(hurricane);
+
+
+        assertEquals(2, lambo.countObservers());
+
+
+        lambo.softwarereleaseUpdates("v10.22.5 - Quick Shifters Update");
+
+        assertTrue(aventador.getReceivedUpdates().contains("v10.22.5 - Quick Shifters Update"));
+        assertTrue(hurricane.getReceivedUpdates().contains("v10.22.5 - Quick Shifters Update"));
+
+        lambo.removeObserver(hurricane);
+        assertEquals(1, lambo.countObservers());
+
+
+        lambo.softwarereleaseUpdates("v11.0.55 - AI Powered Voice Controls");
+
+        assertTrue(aventador.getReceivedUpdates().contains("v11.0.55 - AI Powered Voice Controls"));
+        assertFalse(hurricane.getReceivedUpdates().contains("v11.0.55 - AI Powered Voice Controls"));
+    }
+
+        @Test
+        void Ferrari_Software_Updates () {
+            CarBrand ferrari = new CarBrand("Ferrari");
+
+            CarModel Spider = new CarModel("Ferrari Spider");
+            CarModel GTB = new CarModel("Ferrari 296 GTB");
+
+            ferrari.addObserver(Spider);
+            ferrari.addObserver(GTB);
+
+
+            assertEquals(2, ferrari.countObservers());
+
+
+            ferrari.softwarereleaseUpdates("v10.22.5 - Quick Shifters Update");
+
+            assertTrue(Spider.getReceivedUpdates().contains("v10.22.5 - Quick Shifters Update"));
+            assertTrue(GTB.getReceivedUpdates().contains("v10.22.5 - Quick Shifters Update"));
+
+
+
+            ferrari.removeObserver(Spider);
+            assertEquals(1, ferrari.countObservers());
+
+
+            ferrari.softwarereleaseUpdates("v11.0.55 - AI Powered Voice Controls");
+
+            assertTrue(GTB.getReceivedUpdates().contains("v11.0.55 - AI Powered Voice Controls"));
+            assertFalse(Spider.getReceivedUpdates().contains("v11.0.55 - AI Powered Voice Controls"));
+        }
+    }
+
