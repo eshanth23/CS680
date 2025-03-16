3de250cbe2bb83a710c5655017a3ccf4811a6c9e
Eshanth Kumar Lal Das <98878506+eshanth23@users.noreply.github.com>
2025-03-16 01:31:51 -0400

    Add files via upload

---
+++ HW4/src/main/umbcs680/observer/FuelEfficiencySensor.java
@@ -0,0 +1,14 @@
+package umbcs680.observer;
+
+public class FuelEfficiencySensor extends Observable<Double> {
+    private double mileage_per_mile; // This was missing
+
+    public void setFuelMileage(double mileage_per_mile) {
+        this.mileage_per_mile = mileage_per_mile;
+        notifyObservers(mileage_per_mile);
+    }
+
+    public double getFuelMileage() {
+        return mileage_per_mile;
+    }
+}
