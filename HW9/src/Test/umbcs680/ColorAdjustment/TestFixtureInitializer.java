5cdf8292436a12ebbaa77e8ae0db40a14f5719e9
Eshanth Kumar Lal Das <98878506+eshanth23@users.noreply.github.com>
2025-04-26 21:29:17 -0400

    Add files via upload

---
+++ HW9/src/Test/umbcs680/ColorAdjustment/TestFixtureInitializer.java
@@ -0,0 +1,13 @@
+package umbcs680.ColorAdjustment;
+
+public class TestFixtureInitializer {
+    protected static Image original;
+
+    public static void createFixture() {
+        original = new Image(1, 3);
+        original.setColor(0, 0, new Color(120, 60, 30));
+        original.setColor(1, 0, new Color(200, 200, 200));
+        original.setColor(2, 0, new Color(30, 90, 150));
+    }
+}
+
