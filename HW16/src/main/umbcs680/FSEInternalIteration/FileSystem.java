89e6ac3c5336045b21bc557f1bb2b7116ef91e3a
Eshanth Kumar Lal Das <98878506+eshanth23@users.noreply.github.com>
2025-05-20 23:33:28 -0400

    Add files via upload

---
+++ HW16/src/main/umbcs680/FSEInternalIteration/FileSystem.java
@@ -0,0 +1,28 @@
+package umbcs680.FSEInternalIteration;
+
+import java.util.LinkedList;
+
+class FileSystem {
+    private static FileSystem instance = null;
+    private LinkedList<Directory> rootDirs;
+
+    private FileSystem() {
+        rootDirs = new LinkedList<>();
+    }
+
+    public static FileSystem getFileSystem() {
+        if (instance == null) {
+            instance = new FileSystem();
+        }
+        return instance;
+    }
+
+    public LinkedList<Directory> getRootDirs() {
+        return rootDirs;
+    }
+
+    public void appendRootDir(Directory root) {
+        this.rootDirs.add(root);
+    }
+}
+
